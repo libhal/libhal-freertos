e24a249bcc5099d8f659a25afad47a68806137ef
Khalil Estell <kammce0@gmail.com>
2024-06-06 13:51:38 -0700

    Split libhal-freertos and FreeRTOS packages
    - Add mp3 player decoding demo
    - Add `hal::freertos::io_waiter`
    - Add `hal::freertos::task`
    - Remove all freertos package related stuff

--- demos/platforms/lpc4078.cpp
+++ demos/platforms/lpc4078.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Google LLC
+// Copyright 2024 Khalil Estell
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -20,10 +20,14 @@
 #include <libhal-armcortex/startup.hpp>
 #include <libhal-armcortex/system_control.hpp>
 #include <libhal-armcortex/systick_timer.hpp>
+#include <libhal-freertos/io_waiter.hpp>
 #include <libhal-lpc40/clock.hpp>
 #include <libhal-lpc40/constants.hpp>
 #include <libhal-lpc40/output_pin.hpp>
+#include <libhal-lpc40/power.hpp>
+#include <libhal-lpc40/stream_dac.hpp>
 #include <libhal-util/enum.hpp>
+#include <libhal-util/static_callable.hpp>
 #include <libhal-util/steady_clock.hpp>
 
 #include "../hardware_map.hpp"
@@ -60,40 +64,58 @@ void usage_fault_handler()
   }
 }
 
+hal::cortex_m::dwt_counter* global_steady_clock = nullptr;
+
 extern "C"
 {
   void vPortSetupTimerInterrupt(void)
   {
-    auto& clock = hal::lpc40::clock::get();
-    auto cpu_frequency = clock.get_frequency(hal::lpc40::peripheral::cpu);
+    auto cpu_frequency = hal::lpc40::get_frequency(hal::lpc40::peripheral::cpu);
     static hal::cortex_m::systick_timer systick(cpu_frequency);
-    if (!systick.schedule(xPortSysTickHandler, 1ms)) {
+    try {
+      systick.schedule(xPortSysTickHandler, 1ms);
+    } catch (...) {
       hal::halt();
     }
   }
 
+  unsigned int _freertos_get_high_resolution_timer_count(void)
+  {
+    return global_steady_clock->uptime();
+  }
+  void _freertos_configure_high_resolution_timer(void)
+  {
+  }
+
+  unsigned int idle_percent = 0;
+
   void vApplicationIdleHook()
   {
-    asm volatile("wfi");
+    while (true) {
+      idle_percent = ulTaskGetIdleRunTimePercent();
+      asm volatile("wfi");
+    }
   }
 }
 
-hal::status initialize_processor()
-{
-  // Handled by picolibc's crt0.s startup code
+std::array<std::uint8_t, 6000> buffer1;
+std::array<std::uint8_t, 6000> buffer2;
 
-  return hal::success();
-}
-
-hal::result<hardware_map> initialize_platform()
+hardware_map_t initialize_platform()
 {
-  auto& clock = hal::lpc40::clock::get();
-  auto cpu_frequency = clock.get_frequency(hal::lpc40::peripheral::cpu);
+  // Change the input frequency to match the frequency of the crystal attached
+  // to the external OSC pins.
+  hal::lpc40::maximum(10.0_MHz);
+
+  auto cpu_frequency = hal::lpc40::get_frequency(hal::lpc40::peripheral::cpu);
+
   static hal::cortex_m::dwt_counter steady_clock(cpu_frequency);
+  static hal::lpc40::output_pin led(1, 10);
+  static hal::freertos::io_waiter waiter;
+  static hal::lpc40::stream_dac_u8 my_dac(waiter);
 
-  static auto led = HAL_CHECK(hal::lpc40::output_pin::get(1, 10));
+  global_steady_clock = &steady_clock;
 
-  // Initialize IVT
   hal::cortex_m::interrupt::initialize<hal::value(hal::lpc40::irq::max)>();
 
   hal::cortex_m::interrupt(hal::value(hal::cortex_m::irq::hard_fault))
@@ -113,9 +135,12 @@ hal::result<hardware_map> initialize_platform()
   hal::cortex_m::interrupt(hal::value(hal::cortex_m::irq::systick))
     .enable(xPortSysTickHandler);
 
-  return hardware_map{
+  return {
     .led = &led,
     .clock = &steady_clock,
+    .dac = &my_dac,
     .reset = []() { hal::cortex_m::reset(); },
+    .pcm8_buffer1 = buffer1,
+    .pcm8_buffer2 = buffer2,
   };
 }
--- demos/platforms/stm32f103re.cpp
+++ demos/platforms/stm32f103re.cpp
@@ -1,4 +1,4 @@
-// Copyright 2023 Google LLC
+// Copyright 2024 Khalil Estell
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -35,13 +35,6 @@ extern "C" void vPortSVCHandler();
 using namespace hal::literals;
 using namespace std::literals;
 
-hal::status initialize_processor()
-{
-  // Handled by picolibc's crt0.s startup code
-
-  return hal::success();
-}
-
 void hard_fault_handler()
 {
   while (true) {
@@ -67,15 +60,27 @@ void usage_fault_handler()
   }
 }
 
+hal::cortex_m::dwt_counter* global_steady_clock = nullptr;
+
 extern "C"
 {
   void vPortSetupTimerInterrupt(void)
   {
     auto cpu_frequency = hal::stm32f1::frequency(hal::stm32f1::peripheral::cpu);
     static hal::cortex_m::systick_timer systick(cpu_frequency);
-    if (!systick.schedule(xPortSysTickHandler, 1ms)) {
+    try {
+      systick.schedule(xPortSysTickHandler, 1ms);
+    } catch (...) {
       hal::halt();
     }
+  }
+
+  unsigned int _freertos_get_high_resolution_timer_count(void)
+  {
+    return global_steady_clock->uptime();
+  }
+  void _freertos_configure_high_resolution_timer(void)
+  {
   }
 
   void vApplicationIdleHook()
@@ -84,12 +89,30 @@ extern "C"
   }
 }
 
-hal::result<hardware_map> initialize_platform()
+class stream_dac : public hal::stream_dac<std::uint8_t>
+{
+public:
+  stream_dac()
+  {
+  }
+
+private:
+  void driver_write(samples) override
+  {
+    // Do nothing
+  }
+};
+
+std::array<std::uint8_t, 1250> buffer1;
+std::array<std::uint8_t, 1250> buffer2;
+
+hardware_map_t initialize_platform()
 {
   auto cpu_frequency = hal::stm32f1::frequency(hal::stm32f1::peripheral::cpu);
   static hal::cortex_m::dwt_counter steady_clock(cpu_frequency);
+  global_steady_clock = &steady_clock;
 
-  static auto led = HAL_CHECK(hal::stm32f1::output_pin::get('C', 13));
+  static hal::stm32f1::output_pin led('C', 13);
 
   hal::cortex_m::interrupt::initialize<hal::value(hal::stm32f1::irq::max)>();
 
@@ -108,9 +131,15 @@ hal::result<hardware_map> initialize_platform()
   hal::cortex_m::interrupt(hal::value(hal::cortex_m::irq::pend_sv))
     .enable(xPortPendSVHandler);
 
-  return hardware_map{
+  // stand in dac until a real one is available
+  static stream_dac dac;
+
+  return {
     .led = &led,
     .clock = &steady_clock,
+    .dac = &dac,
     .reset = []() { hal::cortex_m::reset(); },
+    .pcm8_buffer1 = buffer1,
+    .pcm8_buffer2 = buffer2,
   };
 }
